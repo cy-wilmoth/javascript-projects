4df03e8d400614b690e480bc0195535004295285
cy-wilmoth <142552781+cy-wilmoth@users.noreply.github.com>
2023-09-17 18:36:01 -0500

    Update part-one-arrays.js

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,5 +1,18 @@
 //Create an array that can hold 4 items name practiceFile.
 
+let practiceFile = [1, 12, "spicy", 1234];
+
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
 
+practiceFile.push("42");
+console.log(practiceFile);
+practiceFile.push("Hello");
+console.log(practiceFile);
+
 //Use a SetValue to add the items "false", and "-4.6" to the array.  Print the array to confirm the changes.
+
+let first = "false";
+let second = -4.6;
+
+practiceFile.push(first, second);
+console.log(practiceFile);
